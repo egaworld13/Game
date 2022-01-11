e36f15775e4a7deedd0637e6a1a61d82da2ccce1
Edgars <egaworld13@users.noreply.github.com>
2022-01-11 22:52:16 +0200

    Initial commit

---
+++ src/com/ega/Main.java
@@ -0,0 +1,38 @@
+package com.ega;
+
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+        {
+            int[] a = {1, 2, 3, 4, 5, 6};
+            int[] b = {1, 2, 3, 4, 5, 6};
+
+            //a
+            Random n = new Random();
+            int randName = n.nextInt(a.length);
+            System.out.println(a[randName]);
+            //b
+            Random m = new Random();
+            int randName1 = m.nextInt(b.length);
+            System.out.println(b[randName1]);
+
+            //Lielāks>mazāks=vienāds.
+            if (randName > randName1) {
+                System.out.println();}
+            else if (randName == randName1) {
+                    System.out.println("Vienāds");}
+                else
+                    System.out.println("Mazāks");
+
+
+
+        }
+
+
+
+
+    }
+}
+
